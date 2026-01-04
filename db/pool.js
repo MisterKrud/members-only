80486400e11f52f959a408610ad465612aa9271e
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2026-01-04 17:36:08 +1100

    reduce textarea width - setup for dev/prod

--- db/pool.js
+++ db/pool.js
@@ -1,13 +1,17 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-//Dev
+const isProd = process.NODE_ENV === 'production';
+if (isProd) {
+    console.log('\x1b[41m%s\x1b[0m', ' RUNNING IN PRODUCTION MODE '); 
+    // This prints white text on a RED background
+} else {
+    console.log('\x1b[42m%s\x1b[0m', ' Running in Development mode ');
+    // This prints white text on a GREEN background
+}
+
 const pool = new Pool({
-  user: process.env.DEV_DB_USER,
-  password: process.env.DEV_DB_PASSWORD,
-  host: process.env.DEV_DB_HOST,
-  port: process.env.DEV_DB_PORT,
-  database: process.env.DEV_DB_NAME,
+ connectionString: isProd ? process.env.PROD_DB_URL : process.env.DEV_DB_URL,
 });
 
 
