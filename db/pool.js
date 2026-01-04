8f1ab02c6a34b6a8bf1846a56da9b709d355e120
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2026-01-04 20:31:51 +1100

    Still trying to deploy

--- db/pool.js
+++ db/pool.js
@@ -1,19 +1,27 @@
-require("dotenv").config();
-const { Pool } = require("pg");
+require('dotenv').config();
+const { Pool } = require('pg');
 
-const isProd = process.NODE_ENV === 'production';
+
+const isProd = process.env.RENDER === 'true' || process.env.NODE_ENV?.trim() === 'production';
+
+let pool;
+
 if (isProd) {
-    console.log('\x1b[41m%s\x1b[0m', ' RUNNING IN PRODUCTION MODE '); 
-    // This prints white text on a RED background
+
+  pool = new Pool({
+    connectionString: process.env.PROD_DB,
+    ssl: {
+      rejectUnauthorized: false, 
+    },
+  });
+  console.log('\x1b[41m%s\x1b[0m', ' >>> PROD: Connected to Neon ');
 } else {
-    console.log('\x1b[42m%s\x1b[0m', ' Running in Development mode ');
-    // This prints white text on a GREEN background
+  // Use local settings for Dev (WSL)
+  pool = new Pool({
+    connectionString: process.env.DEV_DB,
+    ssl: false
+  });
+  console.log('\x1b[42m%s\x1b[0m', ' >>> DEV: Connected to Local ');
 }
-
-const pool = new Pool({
- connectionString: isProd ? process.env.PROD_DB_URL : process.env.DEV_DB_URL,
-ssl: process.env.NODE_ENV?.trim() === 'production' ? { rejectUnauthorized: false } : false
-});
-
 
 module.exports = pool;
